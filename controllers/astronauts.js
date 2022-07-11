bf88e9e88d71da82d4e295a3e015f8c5296bf3d2
Mitchell Raznick <mraznick@Mitchells-MacBook-Air.local>
2022-07-11 11:40:41 -0400

    completed getAstronaut controller function to find astronaut by ID

--- controllers/astronauts.js
+++ controllers/astronauts.js
@@ -9,3 +9,17 @@ export const getAstronauts = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const getAstronaut = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const astronaut = await Astronaut.findById(id);
+    if (astronaut) {
+      return res.json(astronaut);
+    }
+    res.status(404).json({ message: "Astronaut does not exist" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
--- routes/astronauts.js
+++ routes/astronauts.js
@@ -4,7 +4,7 @@ import * as controllers from "../controllers/astronauts.js"
 const router = Router();
 
 router.get("/astronauts", controllers.getAstronauts)
-// router.get("/astronauts/:id", controllers.getAstronaut)
+router.get("/astronauts/:id", controllers.getAstronaut)
 // router.post("/astronauts", controllers.createAstronaut)
 // router.put("/astronauts/:id", controllers.updateAstronaut)
 // router.delete("/astronauts/:id", controllers.deleteAstronaut)
