b10dbb16531cab5897c02839531058e4ea4f48cd
Mitchell Raznick <mraznick@Mitchells-MacBook-Air.local>
2022-07-12 11:26:30 -0400

    updated server.js for deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import logger from "morgan";
 import chalk from "chalk";
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json())
 app.use(cors())
